4d4b2199942c5c0a3e72d4a549ea24ada15b2215
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-24 19:55:25 -0400

    Create partial class for FullCalendar component

---
+++ src/FullCalendarBlazor/FullCalendar.razor.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace FullCalendarBlazor
+{
+    public partial class FullCalendar
+    {
+        [Inject]
+        private IFullCalendarJsInterop JsInterop { get; set; }
+        
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JsInterop.Render();
+            }
+        }
+    }
+}
