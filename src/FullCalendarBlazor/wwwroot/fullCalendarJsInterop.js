e5ac055b42e457475a8894a7350da6794a7c0f5a
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-24 19:51:47 -0400

    Add render method to jsInterop

---
+++ src/FullCalendarBlazor/wwwroot/fullCalendarJsInterop.js
@@ -0,0 +1,7 @@
+﻿import { FullCalendar } from "./fullcalendar/main.min.js";
+
+export function render() {
+    var calendarElement = document.getElementById('fullcalendar');
+    var calendar = new FullCalendar.Calendar(calendarElement);
+    calendar.render();
+}
