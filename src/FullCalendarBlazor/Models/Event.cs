10a6c39be595f4efc6a0148b920cd2677488615e
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-25 15:35:33 -0400

    Create Event class

--- src/FullCalendarBlazor/Models/Event.cs
+++ src/FullCalendarBlazor/Models/Event.cs
@@ -1,7 +1,41 @@
-﻿namespace FullCalendarBlazor.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace FullCalendarBlazor.Models
 {
+    /// <summary>
+    /// See https://fullcalendar.io/docs/event-parsing for more information.
+    /// </summary>
     public class Event
     {
-
+        public string Id { get; set; }
+        public string GroupId { get; set; }
+        public bool? AllDay { get; set; }
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+        public IEnumerable<DayOfWeek> DaysOfWeek { get; set; }
+        public TimeSpan? StartTime { get; set; }
+        public TimeSpan? EndTime { get; set; }
+        public DateTime? StartRecur { get; set; }
+        public DateTime? EndRecur { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string ClassName { get; set; }
+        public IEnumerable<string> ClassNames { get; set; }
+        public bool? Editable { get; set; }
+        public bool? StartEditable { get; set; }
+        public bool? DurationEditable { get; set; }
+        public bool? ResourceEditable { get; set; } // Requires one of the resource plugins
+        public string ResourceId { get; set; } // Requires one of the resource plugins
+        public List<string> ResourceIds { get; set; } // Requires one of the resource plugins
+        public string Display { get; set; }
+        public bool? Overlap { get; set; }
+        public string Constraint { get; set; }
+        public string Color { get; set; }
+        public string BackgroundColor { get; set; }
+        public string BorderColor { get; set; }
+        public string TextColor { get; set; }
+        public string Rrule { get; set; }
+        public object ExtendedProps { get; set; }
     }
 }
