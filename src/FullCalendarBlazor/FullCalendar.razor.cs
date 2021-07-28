7b1995f07fc422ee39a6651f19e4dc82ea6ec496
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-27 22:49:52 -0400

    Add OnEventMouseEnter and OnEventMouseLeave delegates

--- src/FullCalendarBlazor/FullCalendar.razor.cs
+++ src/FullCalendarBlazor/FullCalendar.razor.cs
@@ -15,11 +15,25 @@ namespace FullCalendarBlazor
         [Parameter] public string Id { get; set; }
         [Parameter] public IEnumerable Events { get; set; }
         [Parameter] public Action<EventActionArgs> OnEventClick { get; set; }
+        [Parameter] public Action<EventActionArgs> OnEventMouseEnter { get; set; }
+        [Parameter] public Action<EventActionArgs> OnEventMouseLeave { get; set; }
 
         [JSInvokable]
         public void EventClick(EventActionArgs e)
         {
             OnEventClick?.Invoke(e);
+        }
+
+        [JSInvokable]
+        public void EventMouseEnter(EventActionArgs e)
+        {
+            OnEventMouseEnter?.Invoke(e);
+        }
+
+        [JSInvokable]
+        public void EventMouseLeave(EventActionArgs e)
+        {
+            OnEventMouseLeave?.Invoke(e);
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
--- src/FullCalendarBlazor/wwwroot/fullCalendarJsInterop.js
+++ src/FullCalendarBlazor/wwwroot/fullCalendarJsInterop.js
@@ -6,6 +6,14 @@ export function render(elementId, serializedData, objRef) {
     calendarData.eventClick = function(info) {
         objRef.invokeMethodAsync('EventClick', info);
     };
+
+    calendarData.eventMouseEnter = function(info) {
+        objRef.invokeMethodAsync('EventMouseEnter', info);
+    };
+
+    calendarData.eventMouseLeave = function(info) {
+        objRef.invokeMethodAsync('EventMouseLeave', info);
+    };
     
     var calendarElement = document.getElementById(elementId);
     var calendar = new FullCalendar.Calendar(calendarElement, calendarData);
