bc987c34e7b39a315f05d6f8c13aa183131259ff
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-27 22:40:31 -0400

    Add eventClick method to calendar info

--- src/FullCalendarBlazor/wwwroot/fullCalendarJsInterop.js
+++ src/FullCalendarBlazor/wwwroot/fullCalendarJsInterop.js
@@ -1,7 +1,13 @@
 ﻿import {FullCalendar} from "./fullcalendar/main.min.js";
 
-export function render(elementId, serializedData) {
+export function render(elementId, serializedData, objRef) {
+    var calendarData = JSON.parse(serializedData);
+    
+    calendarData.eventClick = function(info) {
+        objRef.invokeMethodAsync('EventClick', info);
+    };
+    
     var calendarElement = document.getElementById(elementId);
-    var calendar = new FullCalendar.Calendar(calendarElement, JSON.parse(serializedData));
+    var calendar = new FullCalendar.Calendar(calendarElement, calendarData);
     calendar.render();
 }
