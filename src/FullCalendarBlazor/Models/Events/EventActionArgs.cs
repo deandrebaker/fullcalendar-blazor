c682727ae75493172234c6a288b450358be779f9
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-27 22:36:34 -0400

    Create EventActionArgs

---
+++ src/FullCalendarBlazor/Models/Events/EventActionArgs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FullCalendarBlazor.Models.Events
+{
+    public class EventActionArgs
+    {
+        public Event Event { get; set; }
+        public object El { get; set; }
+        public object JsEvent { get; set; }
+        public object View { get; set; }
+    }
+}
