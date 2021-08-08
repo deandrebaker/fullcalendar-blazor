4e961503233066a5ec85539adad46d3b151dab66
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-08-08 15:50:48 -0400

    Add extension for building toolbar options

---
+++ src/FullCalendarBlazor/Models/Display/ToolbarExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FullCalendarBlazor.Models.Display
+{
+    public static class ToolbarExtensions
+    {
+        public static IEnumerable<ToolbarOption> AddAttached(this IEnumerable<ToolbarOption> toolbarOptions, ToolbarOption toolbarOption)
+        {
+            return toolbarOptions.Append(ToolbarOption.Comma).Append(toolbarOption);
+        }
+
+        public static IEnumerable<ToolbarOption> AddDetached(this IEnumerable<ToolbarOption> toolbarOptions, ToolbarOption toolbarOption)
+        {
+            return toolbarOptions.Append(ToolbarOption.Space).Append(toolbarOption);
+        }
+    }
+}
